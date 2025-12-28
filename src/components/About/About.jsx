fa39d588ebdfe9f50a351fc7b6dd9001f6d2d53e
kazim mehdi saiyed <106967642+kazim02@users.noreply.github.com>
2025-12-28 17:14:32 +0530

    Rename SCSS import to About.scss

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import "./about.scss";
+import "./About.scss";
 
 const About = () => {
   const [openCategory, setOpenCategory] = useState("frontend");
